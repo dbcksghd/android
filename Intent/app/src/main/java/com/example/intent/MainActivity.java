52f70ef8a8fc915d51f143d27cc5e17729ddaff9
Yoochanhong <chanhongy6@naver.com>
2022-07-03 12:54:18 +0900

    Intent 공부한거

--- Intent/app/src/main/java/com/example/intent/MainActivity.java
+++ Intent/app/src/main/java/com/example/intent/MainActivity.java
@@ -24,12 +24,15 @@ public class MainActivity extends AppCompatActivity {
         btn_move = findViewById(R.id.btn_move);
         btn_move.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                str = et_test.getText().toString();
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                intent.putExtra("str", str);
-                startActivity(intent);
+            public void onClick(View view) {
+                str = et_test.getText().toString();//et_test에서 받아온 값을 문자열로 변환해서 str에 저장
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                intent.putExtra("str", str);//인텐트 보내기 전에 str울 실어주는 역할
+                startActivity(intent); //액티비티 이동
             }
         });
     }
 }
+
+//메인액티비티에서 글을 적음->이동버튼 누름->intent에 의해서 액티비티로 이동, putextra에 의해서 적은 내용을 인텐트에 넣어서 이동->
+//서브액티비티에 도착->보낸 글을 겟 인텐트에서 받음->string 형으로 변환->setText로 보임
