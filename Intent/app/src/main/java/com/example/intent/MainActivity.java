270b7462976996469eaab78218f31c5b92350502
Yoochanhong <chanhongy6@naver.com>
2022-07-03 09:42:20 +0900

    Intent 공부한거

---
+++ Intent/app/src/main/java/com/example/intent/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_move;
+    private EditText et_test;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et_test = findViewById(R.id.et_test);
+
+        btn_move = findViewById(R.id.btn_move);
+        btn_move.setOnClickListener();
+    }
+}
