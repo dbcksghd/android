c7fc42bebafcb4a55291f9cce649830c248c885a
데탑 유찬홍 <chanhongy6@naver.com>
2022-08-09 17:12:42 +0900

    인텐트 공부한거

--- Intent/app/src/main/java/com/example/intent/SubActivity.java
+++ Intent/app/src/main/java/com/example/intent/SubActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class SubActivity extends AppCompatActivity {
+
     private TextView tv_sub;
 
     @Override
@@ -16,10 +17,10 @@ public class SubActivity extends AppCompatActivity {
 
         tv_sub = findViewById(R.id.tv_sub);
 
-        Intent intent = getIntent();
-        String str = intent.getStringExtra("str");
 
-        tv_sub.setText(str);
+        Intent intent = getIntent(); //보내는 인텐트 받기
+        String str = intent.getStringExtra("str"); //String str에 받아서 사용할 예정
 
+        tv_sub.setText(str); //textView에 intent로 받은 값을 저자한 str을 표시함
     }
 }
