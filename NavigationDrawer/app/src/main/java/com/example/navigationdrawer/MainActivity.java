abcb1e135d748a2864cd7f32c1871d21bfdd5f53
데탑 유찬홍 <chanhongy6@naver.com>
2022-08-11 18:47:55 +0900

    네비게이션 드로어

--- NavigationDrawer/app/src/main/java/com/example/navigationdrawer/MainActivity.java
+++ NavigationDrawer/app/src/main/java/com/example/navigationdrawer/MainActivity.java
@@ -1,28 +1,43 @@
 package com.example.navigationdrawer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.customview.widget.Openable;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+    NavigationView navigation;
     Button btnOpen;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnOpen = (Button)findViewById(R.id.btnOpen);
+        drawerLayout = findViewById(R.id.drawer);
+        navigation = findViewById(R.id.navigation);
+        btnOpen = findViewById(R.id.btnOpen);
         btnOpen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                drawerLayout.openDrawer(navigation);
             }
         });
 
 
+    }
+    @Override
+    public void onBackPressed() {
+        drawerLayout.closeDrawer(navigation);
     }
 }
