a1abe2bfd73d55023177b5969a1f53e78c6d241e
데탑 유찬홍 <chanhongy6@naver.com>
2022-08-15 23:55:31 +0900

    방향전환 이벤트 처리

--- SampleOrientation/app/src/main/java/com/example/sampleorientation/MainActivity.java
+++ SampleOrientation/app/src/main/java/com/example/sampleorientation/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sampleorientation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    public void showToast(String data){
+        Toast.makeText(this, data, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart () {
+        super.onStart();
+
+        showToast("onStart 호출함");
+
+   }
+
+
+    @Override
+    protected void onStop () {
+        super.onStop();
+
+        showToast("onStop 호출함");
+    }
+
+    @Override
+    protected void onDestroy () {
+        super.onDestroy();
+
+        showToast("onDestroy 호출함");
     }
 }
