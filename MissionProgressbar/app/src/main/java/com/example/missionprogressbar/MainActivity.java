07074aaa371e1d1fc8c39f51a2204e7a6bd0691e
데탑 유찬홍 <chanhongy6@naver.com>
2022-08-16 17:50:40 +0900

    doit 미션 6번

---
+++ MissionProgressbar/app/src/main/java/com/example/missionprogressbar/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.missionprogressbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    SeekBar seekBar;
+    ProgressBar progressBar;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+        editText = findViewById(R.id.editText);
+
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                progressBar.setProgress(i);
+                editText.setText(String.valueOf(i));
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+    }
+}
